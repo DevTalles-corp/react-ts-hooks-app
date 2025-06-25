6c55cf11b534f4d71a23c3fedb020495eab8b4a9
Fernando Herrera <fernando.herrera85@gmail.com>
2025-06-25 10:20:02 -0400

    feat: add Zod validation for task state in getTasksInitialState function

--- src/05-useReducer/reducer/tasksReducer.ts
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -1,3 +1,5 @@
+import * as z from 'zod/v4';
+
 interface Todo {
   id: number;
   text: string;
@@ -16,6 +18,19 @@ export type TaskAction =
   | { type: 'TOGGLE_TODO'; payload: number }
   | { type: 'DELETE_TODO'; payload: number };
 
+const TodoSchema = z.object({
+  id: z.number(),
+  text: z.string(),
+  completed: z.boolean(),
+});
+
+const TaskStateScheme = z.object({
+  todos: z.array(TodoSchema),
+  length: z.number(),
+  completed: z.number(),
+  pending: z.number(),
+});
+
 export const getTasksInitialState = (): TaskState => {
   const localStorageState = localStorage.getItem('tasks-state');
 
@@ -28,8 +43,21 @@ export const getTasksInitialState = (): TaskState => {
     };
   }
 
+  // Validar mediante Zod
+  const result = TaskStateScheme.safeParse(JSON.parse(localStorageState));
+
+  if (result.error) {
+    console.log(result.error);
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };
+  }
+
   // ! Cuidado, porque el objeto puede haber sido manipulado
-  return JSON.parse(localStorageState);
+  return result.data;
 };
 
 export const taskReducer = (
