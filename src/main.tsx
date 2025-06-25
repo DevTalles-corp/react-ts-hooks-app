910d375ffb2c0c0cb3fd7fbeb78520a0d604490c
Fernando Herrera <fernando.herrera85@gmail.com>
2025-06-25 14:04:59 -0400

    feat: enhance ScrambleWords component with confetti effect on points update and import LeafyGreen icon

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { createRoot } from 'react-dom/client';
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
 // import { PokemonPage } from './03-examples/PokemonPage';
 // import { FocusScreen } from './04-useRef/FocusScreen';
-// import { TasksApp } from './05-useReducer/TaskApp';
+import { TasksApp } from './05-useReducer/TaskApp';
 import { ScrambleWords } from './05-useReducer/ScrambleWords';
 
 import './index.css';
