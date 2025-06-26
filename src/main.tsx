ba5340f471d63cdfe79836882336a33e4bd8cbf3
Fernando Herrera <fernando.herrera85@gmail.com>
2025-06-26 14:59:27 -0400

    feat: add MemoCounter and MemoHook components with memoization and performance optimizations

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,10 @@ import { createRoot } from 'react-dom/client';
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
 // import { PokemonPage } from './03-examples/PokemonPage';
 // import { FocusScreen } from './04-useRef/FocusScreen';
-import { TasksApp } from './05-useReducer/TaskApp';
-import { ScrambleWords } from './05-useReducer/ScrambleWords';
+// import { TasksApp } from './05-useReducer/TaskApp';
+// import { ScrambleWords } from './05-useReducer/ScrambleWords';
+// import { MemoHook } from './06-memos/MemoHook';
+import { MemoCounter } from './06-memos/MemoCounter';
 
 import './index.css';
 
@@ -21,6 +23,8 @@ createRoot(document.getElementById('root')!).render(
     {/* <PokemonPage /> */}
     {/* <FocusScreen /> */}
     {/* <TasksApp /> */}
-    <ScrambleWords />
+    {/* <ScrambleWords /> */}
+    {/* <MemoHook /> */}
+    <MemoCounter />
   </StrictMode>
 );
